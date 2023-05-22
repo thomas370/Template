de2c01968eeefc72f9b9f38551f151d017ec7ffa
thomas370 <thomasbortolato5@gmail.com>
2023-05-22 18:33:05 +0200

    last

--- client/src/routes/App.js
+++ client/src/routes/App.js
@@ -1,7 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Admin from "../pages/Admin";
+import Login from "../pages/Login";
 function App() {
   return (
-    <div className="App">
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/Admin" element={<Admin />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      </Router>
   );
 }
 
